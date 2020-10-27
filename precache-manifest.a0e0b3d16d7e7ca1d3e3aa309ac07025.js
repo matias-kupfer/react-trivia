e8143c3796b8b7286ba83e1cef9257460ce722ff
mmatiascfgm <matiascfgm@gmail.com>
2020-10-27 02:43:44 +0100

    Updates

---
+++ precache-manifest.a0e0b3d16d7e7ca1d3e3aa309ac07025.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d737227ba4a9d98c33c9f9a1749fbd2f",
+    "url": "/react-trivia/index.html"
+  },
+  {
+    "revision": "7f5edfd47bad19f7664a",
+    "url": "/react-trivia/static/css/main.d2ec92ae.chunk.css"
+  },
+  {
+    "revision": "54353dd01c550bc9884a",
+    "url": "/react-trivia/static/js/2.d415694a.chunk.js"
+  },
+  {
+    "revision": "9cf9d893b5c51de0aa6dd9e07be64537",
+    "url": "/react-trivia/static/js/2.d415694a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7f5edfd47bad19f7664a",
+    "url": "/react-trivia/static/js/main.14fcd86f.chunk.js"
+  },
+  {
+    "revision": "6951e0fcab12b25d047b",
+    "url": "/react-trivia/static/js/runtime-main.2d137bbc.js"
+  }
+]);
